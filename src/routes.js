e36e5f127c155821c6c61a7044c2bebce3bad18c
MO27 <MO27@MO27.local>
2019-10-02 13:16:46 -0400

    adding resources to the src directory

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './Hoc/Layout';
+
+const Routes = () => {
+    return (
+        <Layout>
+            Hello World
+        </Layout>
+
+    )
+}
+export default Routes;
