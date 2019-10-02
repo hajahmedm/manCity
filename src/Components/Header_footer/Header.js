d525d800916b223b696f3a37e2021e6e88f03f20
MO27 <MO27@MO27.local>
2019-10-02 15:09:06 -0400

    component update

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -4,6 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
 import { Link } from 'react-router-dom';
+import { CityLogo } from '../ui/icons';
 
 class Header extends Component {
     render() {
@@ -18,10 +19,15 @@ class Header extends Component {
                 }}
 
             >
-                <Toolbar styles={{ display: 'flex' }}>
-                    <div styles={{ flexGrow: 1 }}>
+                <Toolbar style={{ display: 'flex' }}>
+                    <div style={{ flexGrow: 1 }}>
                         <div className='header_logo'>
-                            LOGO
+                            <CityLogo
+                                link={true}
+                                linkTo="/"
+                                width='70px'
+                                height='70px'
+                            />
                         </div>
                     </div>
                     <Link to='/the_team'>
